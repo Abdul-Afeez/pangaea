62c38bae5e6a8deb5bb518e60e6229c9f385e3d4
abdulhafeez <abodunrinafeezlekan@gmail.com>
2021-12-21 06:43:40 +0100

    Set up basic blank project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import store from "./Store/Reducers";
+import {Provider} from "react-redux";
+import {AppRoot} from "./components/AppRoot";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return <Provider store={store}>
+    <AppRoot />
+  </Provider>
+
 }
 
 export default App;
+// eslint-disable-next-line no-unused-vars
