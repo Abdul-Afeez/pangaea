4f174bab11be647f58c68a32bed644c85359a120
abdulhafeez <abodunrinafeezlekan@gmail.com>
2021-12-22 22:28:22 +0100

    fixed attribute error

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -17,19 +17,19 @@ export function _NavBar(props) {
         <div className="nav-box grey-bg d-flex align-items-center ps-3 pe-5 justify-content-between">
             <ul>
                 <li>
-                    <a href="#">
+                    <a href="/#">
                         <img className="logo" src="https://www.luminskin.com/_next/static/images/logo-20c2cb1d9d2bb6d2139d0e5cec3103bd.png" alt=""/>
                     </a>
                 </li>
                 <li>
-                    <a className="deep-green-color" href="#">Shop</a>
+                    <a className="deep-green-color" href="/#">Shop</a>
                 </li>
-                <li><a className="deep-green-color" href="#">Learn</a></li>
+                <li><a className="deep-green-color" href="/#">Learn</a></li>
             </ul>
             <ul>
-                <li><a className="deep-green-color" href="#">Account</a></li>
+                <li><a className="deep-green-color" href="/#">Account</a></li>
                 <li className="position-relative" onClick={openCart}>
-                    <a className="deep-green-color" href="#" >
+                    <a className="deep-green-color" href="/#" >
                     <img src={CartIcon} alt=""/>
                     </a>
                     <span className="cart-size cursor-pointer">{cartSize}</span>
